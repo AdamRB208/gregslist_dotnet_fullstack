ba18a7cbd5d0e18cd0004edf42dc52408c2084bb
AdamRB208 <adamrborup@gmail.com>
2025-07-11 15:23:04 -0600

    Created Car Model

---
+++ server/Models/Cars.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace gregslist_dotnet_fullstack.Models;
+
+public class Car
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  [MinLength(3), MaxLength(30)] public string Make { get; set; }
+  public string Model { get; set; }
+  [Range(1896, 2026)] public int Year { get; set; }
+  public string Color { get; set; }
+  public int? Price { get; set; }
+  public int Mileage { get; set; }
+  public string EngineType { get; set; }
+  [Url, MaxLength(1000)] public string ImgUrl { get; set; }
+  public bool? HasCleanTitle { get; set; }
+  public string CreatorId { get; set; }
+  public Account Creator { get; set; }
+}
