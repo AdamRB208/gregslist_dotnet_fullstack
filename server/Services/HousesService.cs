237058410eafd6a66ee6d785440af9963e61946f
AdamRB208 <adamrborup@gmail.com>
2025-07-14 14:29:02 -0600

    Created Houses service

--- server/Services/HousesService.cs
+++ server/Services/HousesService.cs
@@ -2,5 +2,13 @@ namespace gregslist_dotnet_fullstack.Services;
 
 public class HousesService
 {
-  
+
+  public HousesService(HousesRepository housesRepository)
+  {
+    _housesRepository = housesRepository;
+  }
+
+  private readonly HousesRepository _housesRepository;
+
+
 }
