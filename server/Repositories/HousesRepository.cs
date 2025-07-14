7c4e168ac23f5ba23ae5cf17df398b93da9fcb2f
AdamRB208 <adamrborup@gmail.com>
2025-07-14 15:26:05 -0600

    Creating houses

--- server/Repositories/HousesRepository.cs
+++ server/Repositories/HousesRepository.cs
@@ -1,5 +1,8 @@
 
 
+
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
 namespace gregslist_dotnet_fullstack.Repositories;
 
 public class HousesRepository
@@ -34,6 +37,23 @@ public class HousesRepository
       house.Creator = account;
       return house;
     }, new { houseId }).SingleOrDefault();
+    return house;
+  }
+
+  internal House CreateHouse(House houseData)
+  {
+    string sql = @"
+    INSERT INTO houses(sqft, bedrooms, bathrooms, imgUrl, description, price, year, levels, creator_id)
+    VALUES (@Sqft, @Bedrooms, @Bathrooms, @ImgUrl, @Description, @Price, @Year, @Levels, @CreatorId);
+    
+    SELECT houses.*, accounts.*
+    FROM houses INNER JOIN accounts ON accounts.id = houses.creator_id WHERE houses.id = LAST_INSERT_ID();";
+
+    House house = _db.Query(sql, (House house, Account account) =>
+    {
+      house.Creator = account;
+      return house;
+    }, houseData).SingleOrDefault();
     return house;
   }
 }
