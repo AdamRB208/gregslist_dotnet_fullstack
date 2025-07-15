bbf76f0f850fb145c8008290655fc2deb20714a8
AdamRB208 <adamrborup@gmail.com>
2025-07-15 15:14:03 -0600

    Create car modal working.

--- client/src/pages/CarsPage.vue
+++ client/src/pages/CarsPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { AppState } from '@/AppState.js';
 import CarsCard from '@/components/CarsCard.vue';
+import CreateCarModal from '@/components/CreateCarModal.vue';
 import { carService } from '@/services/CarService.js';
 import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
@@ -8,6 +9,7 @@ import { computed, onMounted } from 'vue';
 
 
 const car = computed(() => AppState.cars)
+const account = computed(() => AppState.account)
 
 onMounted(() => {
   getCars()
@@ -30,8 +32,9 @@ async function getCars() {
     <div class="row">
       <div class="d-flex justify-content-between mt-2">
         <h1>Cars</h1>
-        <span class="d-flex justify-content-center">
-          <button class="btn btn-outline-vue" type="button">Create a Car Listing</button>
+        <span v-if="account" class="d-flex justify-content-center">
+          <button class="btn btn-outline-vue" type="button" data-bs-toggle="modal"
+            data-bs-target="#CreateCarModal">Create a Car Listing</button>
         </span>
       </div>
       <div v-for="car in car" :key="car.id" class="col-12 mt-4">
@@ -39,6 +42,7 @@ async function getCars() {
       </div>
     </div>
   </section>
+  <CreateCarModal />
 </template>
 
 
